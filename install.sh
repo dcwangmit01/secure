1c318b5208d7268684a48715b4cc97a5b488eed6
David Wang <dcwangmit01@gmail.com>
2016-05-21 19:27:05 -0700

    fix for darwin

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
-
 #!/bin/bash
 
 # install via:
 #   curl -s https://raw.githubusercontent.com/dcwangmit01/secure/master/install.sh | sudo bash
 
-sudo apt-get update
-sudo apt-get -y install gnupg
+# Take care of dependencies, and do nothing if deps are not there
+if [ `uname` = "Darwin" ]; then
+    if ! which gpg; then
+	echo "Please install program gpg (gnupg)";
+	exit 0;
+    fi
+fi
+if [ `uname` = "Linux" ]; then
+    sudo apt-get update;
+    sudo apt-get -y install gnupg;
+fi
+
 curl -s https://raw.githubusercontent.com/dcwangmit01/secure/master/secure > /usr/local/bin/secure
 sudo chmod 755 /usr/local/bin/secure
